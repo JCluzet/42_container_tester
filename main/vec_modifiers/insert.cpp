4faf227103be4c664645f9a332b5cee731d9f6ba
JCluzet <jo@cluzet.fr>
2022-03-19 00:25:39 +0100

    push

--- main/vec_modifiers/insert.cpp
+++ main/vec_modifiers/insert.cpp
@@ -6,7 +6,7 @@
 /*   By: jcluzet <jcluzet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 19:52:50 by jcluzet           #+#    #+#             */
-/*   Updated: 2022/03/19 00:17:56 by jcluzet          ###   ########.fr       */
+/*   Updated: 2022/03/19 00:25:23 by jcluzet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int main()
     // insert value 9 at position iterator
     vector<int> s3(5,2);
     vector<int>::iterator it2 = s3.begin() + 1;
-    s3.insert(it2, 9);
+    //s3.insert(it2, 9);
     print_vec(s3);
 
     std::cout << std::endl;
@@ -36,7 +36,7 @@ int main()
     vector<int> s4(5,2);
     vector<int>::iterator it3 = s4.begin() + 1;
     vector<int>::iterator it3r = s4.begin() + 1;
-    s4.insert(it3, s3.begin(), s3.end());
+    //s4.insert(it3, s3.begin(), s3.end());
     print_vec(s4);
 
     std::cout << std::endl;
@@ -45,7 +45,7 @@ int main()
     s2.insert(s2.begin(), 1);
     s2.insert(s2.begin() + 1, 2);
     s2.insert(s2.begin() + 2, 212121);
-    s2.insert(it3, s3.begin(), s3.end());
+    s2.insert(s2.begin(), s2.begin(), s2.end());
     
     print_vec(s2);
     return(0);
