5e9b3e9d9b4165681d933450166381f3d5ac7d80
JCluzet <jo@cluzet.fr>
2022-03-19 22:30:30 +0100

    push

--- main/alex_test/insert.cpp
+++ main/alex_test/insert.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
+	vector<int> o(4,4);
 	vector<int> q(3, 0);
 	print_vec(q);
 	std::cout << "*(q.insert(q.begin(), 4)) = " << std::to_string(*(q.insert(q.begin(), 1))) << std::endl;
@@ -10,7 +11,8 @@ int main()
 	q.insert(q.begin() + 2, 2, 2);
 	print_vec(q);
 	std::cout << "q.insert(q.end(), 2, 2)" << std::endl;
-	q.insert(q.end(), q.begin(), q.end());
+	// testing insert(iterator position, InputIterator first, InputIterator last)
+	q.insert(q.end(), o.begin(), o.end());
 	print_vec(q);
 	std::cout << std::endl;
 }
