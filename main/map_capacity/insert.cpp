9720f5225525df3dec7a6bbfe581dab1ae2df6b2
JCluzet <jo@cluzet.fr>
2022-04-08 18:02:40 +0200

    testing third insert of map complete

--- main/map_capacity/insert.cpp
+++ main/map_capacity/insert.cpp
@@ -1,19 +1,33 @@
 #include "../main.hpp"
 
-int     main(){
-map<int, int> m;
-m.insert(pair<int, int>(1, 1));
-m.insert(pair<int, int>(3, 3));
-srand (time(NULL));
-std::cout << "first constructor  : "; 
-for (int i = 0; i < 100; i++){
-    std::cout << (*(m.insert(pair<int, int>(i, i)).first)).second << " ";
-}
+int main()
+{
+    map<int, int> m;
+    m.insert(pair<int, int>(1, 1));
+    m.insert(pair<int, int>(3, 3));
+    srand(time(NULL));
+    std::cout << "first constructor  : ";
+    for (int i = 0; i < 100; i++)
+    {
+        std::cout << (*(m.insert(pair<int, int>(i, i)).first)).second << " "; // test first insert
+    }
 
-std::cout << std::endl;
-std::cout << "second constructor : "; 
-for (int i = 0; i < 100; i++){
-std::cout << (*(m.insert(m.begin(), pair<int, int>(i, i)))).second << " ";
-}
-std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << "second constructor : ";
+    for (int i = 0; i < 100; i++)
+    {
+        std::cout << (*(m.insert(m.begin(), pair<int, int>(i, i)))).second << " "; // test second insert
+    }
+    std::cout << "second constructor : ";
+    m.insert(m.begin(), m.end());            // test third insert
+    for (int i = 0; i < 100; i++)
+    {
+        std::cout << m[i] << " ";
+    }
+    m.insert(m.begin(), m.end());            // test again third insert
+    for (int i = 0; i < 200; i++)
+    {
+        std::cout << m[i] << " ";
+    }
+    std::cout << std::endl;
 }
