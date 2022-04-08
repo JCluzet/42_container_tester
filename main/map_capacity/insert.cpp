c86ba2a54008f1fde085083420a3d17ed3490d4f
JCluzet <jo@cluzet.fr>
2022-04-08 17:36:13 +0200

    now testing first & second insert

--- main/map_capacity/insert.cpp
+++ main/map_capacity/insert.cpp
@@ -2,19 +2,18 @@
 
 int     main(){
 map<int, int> m;
-m.insert(pair<int, int>(1, 1));   // first constructor of insert
+m.insert(pair<int, int>(1, 1));
 m.insert(pair<int, int>(3, 3));
-int r = 0;
 srand (time(NULL));
+std::cout << "first constructor  : "; 
 for (int i = 0; i < 100; i++){
-    r = i;
-    m.insert(pair<int, int>(r, r));
-    std::cout << m[r] << std::endl;
+    std::cout << (*(m.insert(pair<int, int>(i, i)).first)).second << " ";
 }
 
-// use of second constructor with iterator
-m.insert(m.begin(), pair<int, int>(2, 4));
-std::cout << m[2] << std::endl;
-std::cout << m[1] << std::endl;
-std::cout << m[3] << std::endl;
+std::cout << std::endl;
+std::cout << "second constructor : "; 
+for (int i = 0; i < 100; i++){
+std::cout << (*(m.insert(m.begin(), pair<int, int>(i, i)))).second << " ";
+}
+std::cout << std::endl;
 }
