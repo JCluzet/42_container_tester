bf2a5617022746703669021d11f6f44b9050ac45
JCluzet <jo@cluzet.fr>
2022-04-09 21:52:59 +0200

    push

--- main/map_operations/lower_bound.cpp
+++ main/map_operations/lower_bound.cpp
@@ -4,15 +4,17 @@ int main()
 {
     map<int, int> m1;
 
-    unsigned int i = 0;
+    unsigned int i = 1;
 
     while ( i < 500 )
     {
-        m1.insert(pair<int, int>(i, i));
+        m1.insert(pair<int, int>(i*5, i*5));
         i++;
     }
 
     // test lower_bound function
-    map<int, int>::iterator it = m1.lower_bound(0);
+    map<int, int>::iterator it = m1.lower_bound(m1.find(i * 5 * 324)->first);
+    it = m1.lower_bound(m1.lower_bound(m1.find(i * 5 * 48)->first)->first);
+    std::cout << it->second << std::endl;
     return(0);
 }
