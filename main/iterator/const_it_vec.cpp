f2137752aeca8f28b9e2155f215375483518ab24
JCluzet <jo@cluzet.fr>
2022-04-22 15:00:16 +0200

    adding compar it

--- main/iterator/const_it_vec.cpp
+++ main/iterator/const_it_vec.cpp
@@ -14,6 +14,15 @@ int main()
 		it++;
 	}
 
+	// test comparison of 2 iterators (const & non-const)
+	vector<char> a;
+	for (int i = 0 ; i < 6; i ++)
+		a.push_back(i + 'a');
+	vector<char>::iterator it1 = a.begin() + 1;
+	vector<char>::const_iterator cit = a.begin() + 2;
+	std::cout << (it1 != cit) << std::endl;
+	std::cout << (it1 > cit) << std::endl;
+
     // std::cout << std::endl;
 	return 0;
 }
